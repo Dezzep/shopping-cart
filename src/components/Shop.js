b138153786ad1c07a9c05914e57ba17e80fb3982
Jacob Handelman <jacobhandelman@gmail.com>
2022-07-18 18:56:05 -0400

    api call for fakestoreapi set in shop.js.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,27 @@
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
 
 const Shop = () => {
+  
+  const [storeData, setStoreData] = useState('');
+  const fetchFakeStoreApi = async () => {
+    const request = await fetch("https://fakestoreapi.com/products/", {mode: 'cors'})
+    const data = await request.json();
+    // setStoreData(data)
+    console.log(data[1])
+    return(data[1]);
+  }
+  useEffect(() => {
+    fetchFakeStoreApi();
+  })
+
   return (
     <div>
       <Navbar />
     <div>
       <h1>Shop</h1>
+      
+      
     </div>
     </div>
   )
---
+++ src/components/ShopCards.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+const Shop = (props) => {
+  
+  return (
+    <div>      
+    </div>
+  )
+}
+export default Shop;
