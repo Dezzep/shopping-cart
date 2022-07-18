4cd05588ccc1660db5a568c91de219aa86febd39
Jacob Handelman <jacobhandelman@gmail.com>
2022-07-18 19:14:15 -0400

    added and styled a card that displays one of the items from the api call on the shop section of the page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,23 +1,34 @@
 import Navbar from "./Navbar";
 import { useEffect, useState } from "react";
+import ShopCards from "./ShopCards";
 
 const Shop = () => {
   
   const [storeData, setStoreData] = useState('');
+  
+
   const fetchFakeStoreApi = async () => {
-    const request = await fetch("https://fakestoreapi.com/products/", {mode: 'cors'})
-    const data = await request.json();
-    // setStoreData(data)
-    console.log(data[1])
-    return(data[1]);
+    if (!storeData) {
+      const request = await fetch("https://fakestoreapi.com/products/", {mode: 'cors'})
+      const data = await request.json();
+      // setStoreData(data)
+      console.log(data[0])
+      setStoreData(data[2]);
+      return(data[0]);
+    }
   }
+  
   useEffect(() => {
     fetchFakeStoreApi();
   })
+  
+  
 
   return (
     <div>
       <Navbar />
+      <ShopCards title={storeData.title} image={storeData.image} description={storeData.description} />
+    
     <div>
       <h1>Shop</h1>
       
--- src/components/ShopCards.js
+++ src/components/ShopCards.js
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react";
-
-const Shop = (props) => {
-  
+const ShopCards = (props) => {
   return (
-    <div>      
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure><img src={props.image} alt={props.title}></img></figure>   
+      <div className="card-body">
+        <h2 className="card-title">{props.title}</h2>
+        <p>{props.description}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Add To Cart</button>
+        </div>
+      </div>
+
     </div>
   )
 }
-export default Shop;
+export default ShopCards;
