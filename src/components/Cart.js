fcbca1493580e84c1007d0c49f38863d0dcfc916
Jacob Handelman <jacobhandelman@gmail.com>
2022-07-20 08:33:14 -0400

    added content to the home page. (hero section), call to action button routes to the shop.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,9 +4,6 @@ const Cart = (props) => {
   const [rerender, setRerender] = useState(props.rendering);
   const [apiCall, setApiCall] = useState(false);
   const [apiData, setApiData] = useState([]);
-  const [cartIdsAndQuantity, setCartIdsAndQuantity] = useState({})
-
-  const test = [];
 
   const fetchFakeStoreApi = async () => {
     if (!apiCall){
@@ -36,35 +33,25 @@ const shopArray = () => {
     const cartIdsString = localStorage.getItem('cartIds');
     const cartIds = JSON.parse(cartIdsString);
     for (const key in cartIds) {
-      array.push(<div key={`${apiData[key].title}__${apiData[key].id}`}>
+      array.push(<div className="mb-2 mt-2 p-6" key={`${apiData[key].title}__${apiData[key].id}`}>
         {apiData[key].title}</div>)
     }
     return (array);
   }
 }
+  const theShop = shopArray();
 
-   const theShop = shopArray();
-// for (const i in storeData) {
-//   arrayOfCards.push(<ShopCards 
-//     title={storeData[i].title} 
-//     key={storeData[i].title} 
-//     image={storeData[i].image} 
-//     idKey={storeData[i].id}
-//     price={storeData[i].price.toFixed(2)} // To fixed makes sure all prices look like ex: 12.3 = 12.30
-//     addToCart={addItem}
-//   />)
-// }
   return (
     <div>
       <div className="absolute right-0 top-28 sm:top-20 p-1  h-full">
           <ul className={`menu h-screen p-4 overflow-y-auto w-80 bg-base-300 border-black border-2 text-base-content ease-in-out duration-500 ${props.display? 'translate-x-0' : 'translate-x-full'}`}>
-            <h1 className="mb-4">Shopping Cart</h1>
-            { JSON.parse(localStorage.getItem('totalCartItems')) || 'Shopping Cart Is Empty!'}
-            {theShop}
-            <div className="sticky bottom-0 ml-auto w-2/4">
-              <button className="btn btn-sm btn-primary mb-2">Checkout</button>
+          <div className="sticky top-0 right-0 flex justify-around mb-4 bg-neutral/50 py-4 ">
+              <button className="btn btn-sm btn-primary">Checkout</button>
               <button className="btn btn-sm btn-danger bg-red-600">Empty Cart</button>
               </div>
+            <h1 className="mb-4">Shopping Cart</h1>
+            { JSON.parse(localStorage.getItem('totalCartItems')) || 'Shopping Cart Is Empty!'}
+            <div className="mb-24">{theShop}</div>
           </ul>
           
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,25 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
+import { Link } from 'react-router-dom';
+
+
 
 const Home = () => {
   const [cartItems] = useState(
     JSON.parse(localStorage.getItem('totalCartItems')) || 0
-  );
+    );
   return (
     <div>
       <Navbar cartDisplay={cartItems} />
-      <div>
-        <h1>Home Page</h1>
+      <div className="hero min-h-screen" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/hero.jpg'})` }} >
+        <div className="hero-overlay bg-opacity-50"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Your Wardrobe is in shambles</h1>
+            <p className="mb-5">It's time for a change.</p>
+            <Link to='/shop'><button className="btn btn-primary">Shop Now</button></Link>
+          </div>
+        </div>
       </div>
     </div>
   )
