122e2599b7d97c150b18a04a26c69ad14d16dc26
Jacob Handelman <jacobhandelman@gmail.com>
2022-07-21 19:45:24 -0400

    the cart now displays the total price of all cart contents.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -30,11 +30,8 @@ const Cart = (props) => {
       };
     }, [ref]);
   }
-
   useOutsideAlerter(cartRef)
 // ---------------------------------------- END OF COPY
-
-
 
 const shopArray = () => {
   if (props.data && props.cartIds) {
@@ -46,18 +43,39 @@ const shopArray = () => {
     return (array);
   }
 }
+const totalPrice = () => {
+    let price = 0;
+  if (props.data && props.cartIds) {
+    for (const key in props.cartIds) {
+      console.log(`${props.data[key].price} + ${props.cartIds[key]}`)
+      price += props.data[key].price * props.cartIds[key]
+    }
+    return `${price.toFixed(2)}$`;
+  }
+
+}
+useEffect(() => {
+  totalPrice()
+})
+
   const theShop = shopArray();
 
   return (
     <div id="shopping_cart" ref={cartRef}>
       <div className="absolute right-0 top-28 sm:top-20 p-1  h-full">
-          <ul className={`menu h-screen p-2 overflow-y-auto w-80 bg-base-300 border-black border-2 text-base-content ease-in-out duration-500 ${props.display? 'translate-x-0' : 'translate-x-full'}`}>
-          <div className="sticky top-0 right-0 flex justify-around mb-4 bg-neutral/50 py-4 z-50">
-              <button className="btn btn-sm btn-primary">Checkout</button>
-              <button className="btn btn-sm btn-danger bg-red-600">Empty Cart</button>
+          <ul className={`menu h-screen p-2 overflow-y-auto w-80 bg-base-300 text-base-content ease-in-out duration-500 ${props.display? 'translate-x-0' : 'translate-x-full'}`}>
+          <div className="sticky top-0 right-0 grid grid-cols-2 mb-4 bg-secondary p-4 py-4 z-50">
+              <button className="btn btn-success hover:bg-primary/40 mr-2">Checkout</button>
+              <button onClick={props.toggleCartWithButton} id="close-cart" className="btn btn-danger bg-red-600">Close</button>
+              <div>
+              <h1 className="mt-4 p-2">Total: {totalPrice()}</h1>
+              <h1 className="p-2"> Total Items: {props.data === undefined ? 0 : props.data}</h1>
               </div>
-            <h1 className="mb-4">Shopping Cart</h1>
-            { JSON.parse(localStorage.getItem('totalCartItems')) || 'Shopping Cart Is Empty!'}
+              </div>
+            <h1 className="mb-4 text-xl font-bold text-center">Your Cart</h1>
+              
+            <h1 className="text-center text-xl text-secondary-content bg-secondary"> {props.data === undefined ? 'Your Cart is Empty!' : null}</h1>
+           
             <div className="mb-24">{theShop}</div>
           </ul>
           
