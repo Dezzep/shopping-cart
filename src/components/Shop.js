9254fb3694af76ef0dbbd570ba103959e586329c
Jacob Handelman <jacobhandelman@gmail.com>
2022-07-22 11:47:49 -0400

    added a loading bar for the api call. removed un-used files

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -18,7 +18,28 @@ const Shop = (props) => {
   }, [props.cartIds])
 
  
-  
+  const loadingBar = () => {
+    if (!props.storeData) {
+      return (
+        <div className="text-center">
+         
+         <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed" disabled="">
+      <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+      <div className="flex flex-col">
+        <p>Fetching Api Data...</p>
+        
+        <p className="text-xs">The Server Might Be Down</p>
+      </div>
+    </button>
+        </div>
+      )
+    } else {
+      return (null)
+    }
+  }
   
   
 const arrayOfCards = [];  
@@ -39,8 +60,8 @@ for (const i in props.storeData) {
     <div className="bg-base-100 mt-36">
       <Navbar blurToggleOn={props.blurToggleOn} blurToggleOff={props.blurToggleOff} deleteItem={props.deleteItem} cartDisplay={props.totalCartItems} addItem={props.addItem} cartIds={props.cartIds} data={props.storeData} />
             {props.shopBlur()}
-
       <h1 className="text-2xl text-center mb-12 z-10">Shop</h1>
+      {loadingBar()}
     <div className="flex flex-wrap justify-center gap-6 sm:ml-12 sm:mr-12 md:ml-12 md:mr-12">
       {arrayOfCards}
     </div>
